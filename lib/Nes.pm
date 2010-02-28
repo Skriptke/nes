cca921a11a02842b9f812f8f88e68c2fd067ab98
Enrique F. Castañón <skriptke@yahoo.es>
2010-02-28 19:52:46 +0100

    Several install fixes

--- lib/Nes.pm
+++ lib/Nes.pm
@@ -26,7 +26,7 @@ use strict;
 # cgi environment no defined in command line
 no warnings 'uninitialized';
 
-our $VERSION          = '1.01_01';
+our $VERSION          = '1.01';
 our $CRLF             = "\015\012";
 our $MAX_INTERACTIONS = 500;
 our $MOD_PERL         = $ENV{'MOD_PERL'} || 0;
